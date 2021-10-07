5a66e1279ab5c701580be5d79532683233a08740
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-10-06 22:52:31 -0700

    added client and server and initialized files

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({
+//   extended: true
+// }));
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(
+  express.urlencoded({
+    limit: "30mb",
+    extended: true,
+  })
+);
+app.use(cors());
