49f33c776cac59468fe95b43001988933f51dc96
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-10-08 21:53:00 -0700

    connected to database

--- server/index.js
+++ server/index.js
@@ -18,3 +18,15 @@ app.use(
   })
 );
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://jeremylentz:Pooppoop42069@cluster0.m1r2t.mongodb.net/MERN-food-app?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
