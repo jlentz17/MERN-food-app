0b4964c07fef1c52d19d9f2f95891c67c3dfa13e
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-10-10 21:15:55 -0700

    connected app to redux and connected css

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,23 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import recipes from "./images/sherman.png";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Stacia's Recipes
         </Typography>
-        <img src={recipes} alt="recipes" height="600" />
+        <img
+          className={classes.image}
+          src={recipes}
+          alt="recipes"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
