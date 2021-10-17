9149f5a543cf4d35946b9f8e36ba25de9c1c067c
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-10-17 12:55:59 -0700

    deployed to heroku

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import postRoutes from "./routes/posts.js";
 const app = express();
 dotenv.config()
 
+
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(
   express.urlencoded({
@@ -17,6 +18,10 @@ app.use(
 app.use(cors());
 
 app.use("/posts", postRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello to Recipes API")
+})
 
 const PORT = process.env.PORT || 5000;
 
